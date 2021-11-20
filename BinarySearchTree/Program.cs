d84c946fd576a78fc384bab53620a5003c61b84e
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-20 18:52:56 +0530

    Create Binary Search Tree Program

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program.");
+            BinaryTree binarytree = new BinaryTree();
+
+            BinaryTree obj1 = new BinaryTree();
+            obj1.Insert(56);
+            obj1.Insert(30);
+            obj1.Insert(70);
+
+            Console.Write("Inorder result : ");
+            obj1.TraverseInorder(obj1.root);
+            Console.WriteLine();
+
+            Console.Write("Post-order result : ");
+            obj1.TraversePostorder(obj1.root);
+            Console.WriteLine();
+
+            Console.Write("Pre-order result : ");
+            obj1.TraversePreorder(obj1.root);
+            Console.WriteLine();
+        }
+    }
+}
